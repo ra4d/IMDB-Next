95e869c10317c03e5467d0f0262aa5a4a2e580af
rashed <admedrishd6@gmail.com>
2024-10-25 03:24:37 +0300

    create a about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function About() {
+  return (
+    <div className={`max-w-6xl mx-auto p-3 space-y-4`}>
+        <h1 className={`text-2xl font-medium text-amber-600`}>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, repellendus eligendi nihil ipsa dignissimos dolor officiis facere voluptas soluta sunt, sequi dolore consequuntur aspernatur fugiat! Perferendis facere, error voluptatum minima blanditiis fugiat cumque quod odit deleniti, repellendus ad reprehenderit numquam accusantium cum corporis nemo. Unde ipsa commodi, facilis mollitia accusamus maiores magni repellat quod eum explicabo tenetur, nihil voluptatem nisi.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, repellendus eligendi nihil ipsa dignissimos dolor officiis facere voluptas soluta sunt, sequi dolore consequuntur aspernatur fugiat! Perferendis facere, error voluptatum minima blanditiis fugiat cumque quod odit deleniti, repellendus ad reprehenderit numquam accusantium cum corporis nemo. Unde ipsa commodi, facilis mollitia accusamus maiores magni repellat quod eum explicabo tenetur, nihil voluptatem nisi.</p>
+    </div>
+  )
+}
